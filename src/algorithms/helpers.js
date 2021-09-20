422d2229ce0fde0b14f47a0a7c8667ef91cd90c4
Spandita Banerjee <spanditabanerjee@gmail.com>
2021-09-20 20:52:33 +0530

    bubble sort

--- src/algorithms/helpers.js
+++ src/algorithms/helpers.js
@@ -1,70 +1,50 @@
-import React from 'react';
-import {
-  swap,
-  newTrace,
-  addToTrace,
-  lastSorted,
-  createKey
-} from './helpers';
-
-const BubbleSort = (nums) => {
-  // Set up code for tracing the algorithm
-  const trace = newTrace(nums);
-
-  // Sorting Algorithm with trace capture
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length - i - 1; j++) {
-      // Visualize: Comparing A[j] and A[j + 1]
-      addToTrace(trace, nums, lastSorted(trace), [j, j + 1]);
-      if (nums[j] > nums[j + 1]) {
-        swap(nums, j, j + 1);
-        // Visualize: Swap A[j] and A[j + 1]
-        addToTrace(trace, nums, lastSorted(trace), [], [j, j + 1]);
-      }
+export const newTrace = (array) => {
+  return [
+    {
+      array: [...array],
+      groupA: [],
+      groupB: [],
+      groupC: [],
+      groupD: [],
+      sortedIndices: []
     }
-
-    // Visualize: final value is sorted
-    addToTrace(trace, nums, [
-      ...lastSorted(trace),
-      nums.length - 1 - i
-    ]);
-  }
-
-  return trace;
+  ];
 };
 
-export const BubbleSortKey = createKey('Comparing', 'Swapping');
-export const BubbleSortDesc = {
-  title: 'Bubble Sort',
-  description: (
-    <p>
-      <a
-        href="https://en.wikipedia.org/wiki/Bubble_sort"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Bubble Sort
-      </a>{' '}
-      is a simple sorting algorithm that repeatedly steps through the
-      list, compares adjacent elements and swaps them if they are in the
-      wrong order.The pass through the list is repeated until the list
-      is sorted. The algorithm, which is a comparison sort, is named for
-      the way smaller or larger elements "bubble" to the top of the
-      list. Although the algorithm is simple, it is too slow and
-      impractical for most problems
-    </p>
-  ),
-  worstCase: (
-    <span>
-      O(n<sup>2</sup>)
-    </span>
-  ),
-  avgCase: (
-    <span>
-      O(n<sup>2</sup>)
-    </span>
-  ),
-  bestCase: <span>O(n)</span>,
-  space: <span>O(1)</span>
+export const addToTrace = (
+  trace,
+  array,
+  sortedIndices = [],
+  groupA = [],
+  groupB = [],
+  groupC = [],
+  groupD = []
+) => {
+  trace.push({
+    array: [...array],
+    groupA: [...groupA],
+    groupB: [...groupB],
+    groupC: [...groupC],
+    groupD: [...groupD],
+    sortedIndices: [...sortedIndices]
+  });
 };
-export default BubbleSort;
+
+export const lastSorted = (trace) => {
+  return trace[trace.length - 1].sortedIndices;
+};
+
+export const swap = (array, i, j) => {
+  const tmp = array[i];
+  array[i] = array[j];
+  array[j] = tmp;
+};
+
+export const createRange = (start, end) => {
+  return [...Array(end - start).keys()].map((elem) => elem + start);
+};
+
+export const createKey = (groupA, groupB, groupC, groupD) => {
+  return { groupA, groupB, groupC, groupD };
+};
+
